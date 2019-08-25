27b31b2baabab07d03e54e71f68c70843abdee7a
BoraCoban <boracoban9@gmail.com>
2019-08-25 21:33:23 +0300

    SignalR-CurrencyTable

---
+++ Currency/Currency/Money.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Currency
+{
+    public class Money
+    {
+        public Money()
+        {
+            this.ID = Guid.NewGuid();
+            this.Value = 0;
+        }
+
+        public Money(string name, decimal value) 
+        {
+            this.ID = Guid.NewGuid();
+            this.Value = 0;
+
+            this.Name = name;
+            this.Value = value;
+        }
+
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+
+        private decimal _value
+        {
+            get;
+            set;
+        } 
+        public decimal Value
+        {
+            get { return _value; }
+            set
+            {
+                LastValue = _value;
+                _value = value;
+            }
+        }
+
+        public decimal LastValue
+        {
+            get;
+            set;
+        }
+        public decimal Change { get; set; }
+    }
+}
---
+++ Currency/Currency/moneyHub.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Currency
+{
+    public static class DB
+    {
+        public static HashSet<Money> Money = new HashSet<Money>()
+        {
+            new Money("Dolar", 5.76m),
+            new Money("EURO", 6.47m),
+            new Money("Ruble", 0.087m),
+            new Money("Yuan", 0.81m),
+            new Money("Sterlin", 7.07m)
+        };
+    }
+
+    public class moneyHub : Hub
+    {
+        public IEnumerable<Money> GetAll()
+        {
+            Money item1 = new Money();
+            Money item2 = new Money("Riyal", 0.8m);
+            return DB.Money.ToList();
+        }
+
+        public void PressValueToAllClients(Guid id, decimal value)
+        {
+            Money money = DB.Money.Where(x => x.ID == id).FirstOrDefault();
+            money.Value = value;
+
+            Clients.Others.newValue(money);
+        }
+    }
+}
